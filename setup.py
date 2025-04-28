ce1de8eae0f291978e0e6f8a79713aa01eda7a9c
cello-phane <cellophane11578@gmail.com>
2025-04-28 05:59:31 -0700

    Add setup.py for compiling pyx module

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import platform
+import os
+
+# Platform-specific settings
+if platform.system() == "Windows":
+    libraries = ["raylib", "opengl32", "gdi32", "winmm"]
+    library_dirs = ["C:/Users/admin/py/cythons/raylib/lib"]
+    include_dirs = ["C:/Users/admin/py/cythons/raylib/include"]
+    extra_compile_args = []
+    extra_link_args = []
+else:  # Linux/Mac
+    libraries = ["raylib", "m"]
+    library_dirs = ["/usr/local/lib64" if "64" in platform.machine() else "/usr/local/lib"]
+    include_dirs = ["/usr/local/include/raylib"]
+    extra_compile_args = []
+    extra_link_args = []
+
+extensions = [
+    Extension(
+        "raylib_wrapper",
+        ["src/raylib_wrapper.pyx"],
+        libraries=libraries,
+        library_dirs=library_dirs,
+        include_dirs=include_dirs,
+        extra_compile_args=extra_compile_args,
+        extra_link_args=extra_link_args
+    )
+]
+
+setup(
+    name="raylib_wrapper",
+    ext_modules=cythonize(extensions)
+)
