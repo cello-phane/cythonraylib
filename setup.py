f026b6340c77c558469e24244397f7b1ded2a894
Michael L <87787413+cello-phane@users.noreply.github.com>
2026-06-11 02:29:10 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,17 @@ static_lib_path = os.path.abspath("libraylib.a")
 
 # Platform-specific settings
 if platform.system() == "Windows":
-    # Add the below if linking with shared(dyanmic) .lib file
+    # Add(Uncomment line) the below if linking with shared(dyanmic) .lib file
     #libraries = ["raylib"] 
     libraries = ["opengl32", "gdi32", "winmm", "user32", "shell32"]
-    library_dirs = ["C:/Users/xxpwb/py/cythonraylib/raylib/lib", "C:/raylib/raylib/src"]
-    include_dirs = ["C:/Users/xxpwb/py/cythonraylib/raylib/include", "C:/raylib/raylib/src"]
+	# Change path according to where raylib lib and include are located
+    library_dirs = ["C:/Users/admin/py/cythonraylib/raylib/lib", "C:/raylib/raylib/src"]
+    include_dirs = ["C:/Users/admin/py/cythonraylib/raylib/include", "C:/raylib/raylib/src"]
     extra_compile_args = []
     extra_link_args = []
 else:  # Linux/Mac
     libraries = ["raylib", "m", "GL"]
+	# Change path according to where raylib lib and include are located, and also X11 if that needs to be specified for linking
     library_dirs = ["/usr/lib64", "/usr/X11R7/lib64", "/usr/local/lib64" if "64" in platform.machine() else "/usr/local/lib"]
     include_dirs = ["/usr/include", "/usr/X11R7/include", "/usr/local/include/raylib"]
     extra_compile_args = []
